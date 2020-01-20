7b5f0ba1b4780d8a7c6b81e282076e75bc185ba3
Crazy Wang <yyywk@outlook.com>
2020-01-20 10:16:18 +0800

    伪造请求头

---
+++ lib/config/httpHeaders.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+const HttpHeaders = {
+  'Accept': 'application/json, text/plain, */*',
+  'Accept-Encoding': 'gzip, deflate, br',
+  'Accept-Language': 'zh-CN,zh;q=0.9',
+  'Connection': 'keep-alive',
+  'Content-Type': 'application/json',
+  'Cookie':
+      'gksskpitn=d6ebe444-b4c5-4f9b-8f1d-7e99eb830437; _ga=GA1.2.2052043008.1579484159; _gid=GA1.2.1080905814.1579484159; MEIQIA_TRACK_ID=1UQ8vIaK1PnzvyU83jm7OKbVvkv; MEIQIA_VISIT_ID=1WdmKBmQEaZ6dGRU8wp2XBQHfxq; GRID=e90cc31-80a49e5-f7ffc8c-5f72954; SERVERID=1fa1f330efedec1559b3abbcb6e30f50|1579487850|1579487777; _gat=1; Hm_lvt_022f847c4e3acd44d4a2481d9187f1e6=1579484160,1579484209,1579484233; Hm_lpvt_022f847c4e3acd44d4a2481d9187f1e6=1579484233',
+  'Host': 'time.geekbang.org',
+  'Origin': 'https://time.geekbang.org',
+  'Referer': 'https://time.geekbang.org/',
+  'Sec-Fetch-Mode': 'cors',
+  'Sec-Fetch-Site': 'same-origin',
+  'User-Agent':
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
+};
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,80 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
-
+import 'package:flutter_shop/config/httpHeaders.dart';
 class HomePage extends StatefulWidget {
-  HomePage({Key key}) : super(key: key);
-
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  TextEditingController typeController = TextEditingController();
-  String showText = "欢迎您来到美好人间";
-
+  String showText = '还没有请求数据';   //声明要显示的数据并且设置默认值
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: Scaffold(
-      appBar: AppBar(
-        title: Text('美好人间'),
-      ),
-      body: SingleChildScrollView(
-        child: Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('请求远程数据'),
+        ),
+        body: SingleChildScrollView(
           child: Column(
-            children: <Widget>[
-              TextField(
-                controller: typeController,
-                decoration: InputDecoration(
-                    //装饰输入文本框
-                    contentPadding: EdgeInsets.all(10),
-                    labelText: '美女类型',
-                    helperText: '请输入你喜欢的类型'),
-                autofocus: false,
-              ),
-              RaisedButton(
-                onPressed: _choiceAction,
-                child: Text("选择完毕"),
-              ),
-              Text(
-                showText,
-                overflow: TextOverflow.ellipsis,
-                maxLines: 1,
-              ),
-            ],
+          children: <Widget>[
+            RaisedButton(
+              onPressed: _geek,
+              child: Text('请求数据'),
+            ),
+            Text(showText)
+          ],
           ),
         ),
-      ),
-    )
+      )
     );
   }
 
-  void _choiceAction() {
-    print('开始选择你喜欢的类型............');
-    if (typeController.text.toString() == '') {
-      showDialog(
-          context: context,
-          builder: (context) => AlertDialog(
-                title: Text('美女类型不能为空'),
-              ));
-    } else {
-      getHttp(typeController.text.toString()).then((val) {
-        setState(() {
-          showText = val['data']['name'].toString();
-        });
+  void _geek(){
+    print("开始向极客时间请求数据.....");
+    getHttp()..then((val){
+      setState(() {
+        showText=val['data'].toString();
       });
-    }
-  }
+    }) ;
+ }
 
-  Future getHttp(String TypeText) async {
-    try {
+  Future getHttp() async{
+    try{
       Response response;
-      var data = {'name': TypeText};
-      response = await Dio().post(
-          "https://easy-mock.com/mock/5e241b8631e8b414fccb3255/example/dabaojian_post",
-          queryParameters: data);
+      Dio dio = new Dio();
+      dio.options.headers = HttpHeaders;
+      response=await dio.get('https://time.geekbang.org/serv/v1/column/labels');
+      print(response);
       return response.data;
-    } catch (e) {
+    }catch(e){
       return print(e);
     }
   }
